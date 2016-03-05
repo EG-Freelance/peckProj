240ca707ec4bd9283dd973c53a50aea538dcdaeb
Erik <erik.w.gibbons@gmail.com>
2016-03-05 18:48:06 +0000

    Fix edit account JS

--- app/assets/javascripts/sign_up.js
+++ app/assets/javascripts/sign_up.js
@@ -1,7 +1,7 @@
 function signUpValidation(){
   // For owner
   $('#owner-reg-form').on('keypress', function(){
-    if((($('#user_preferred_payment').val() === "Paypal" && $('#owner-paypal-acct').val() === "") || ($('#user_preferred_payment').val() === "Venmo" && $('#owner-venmo-acct').val() === "")) || $('#owner-email').val() === "" || $('#owner-password').val() === "" || $('#owner-confirm-password').val() === ""){
+    if((($('#owner-preferred-payment').val() === "Paypal" && $('#owner-paypal-acct').val() === "") || ($('#owner-preferred-payment').val() === "Venmo" && $('#owner-venmo-acct').val() === "")) || $('#owner-email').val() === "" || $('#owner-password').val() === "" || $('#owner-confirm-password').val() === ""){
       $('#owner-signup').addClass('disabled');
       $('#owner-reg-form').on("keypress", function (e) {
         if (e.keyCode == 13) {
@@ -38,5 +38,50 @@ function signUpValidation(){
   });
 }
 
-$(document).on('page:load', signUpValidation);
-$('document').ready(signUpValidation);
+function editValidation(){
+  // For owner
+  $('#owner-edit-form').on('keypress', function(){
+    if((($('#owner-edit-preferred-payment').val() === "Paypal" && $('#owner-edit-paypal-acct').val() === "") || ($('#owner-edit-preferred-payment').val() === "Venmo" && $('#owner-edit-venmo-acct').val() === "")) || $('#owner-edit-email').val() === "" || $('#owner-old-password').val() === ""){
+      $('#owner-update').addClass('disabled');
+      $('#owner-edit-form').on("keypress", function (e) {
+        if (e.keyCode == 13) {
+          return false;
+        }
+      });
+    }else{
+      $('#owner-update').removeClass('disabled');
+      $('#owner-edit-form').on("keypress", function (e) {
+        if (e.keyCode == 13) {
+          $('#owner-update').trigger('click');
+        }
+      });
+    }    
+  });
+  
+  // For guest
+  $('#guest-edit-form').on('keypress', function(){
+    if($('#guest-email').val() === "" || $('#guest-old-password').val() === "" || $('#guest-owner-email').val() === ""){
+      $('#guest-update').addClass('disabled');
+      $('#guest-edit-form').on("keypress", function (e) {
+        if (e.keyCode == 13) {
+          return false;
+        }
+      });
+    }else{
+      $('#guest-update').removeClass('disabled');
+      $('#guest-edit-form').on("keypress", function (e) {
+        if (e.keyCode == 13) {
+          $('#guest-update').trigger('click');
+        }
+      });
+    }    
+  });
+}
+
+function allValidation(){
+  signUpValidation();
+  editValidation();
+}
+
+$(document).on('page:load', allValidation);
+$('document').ready(allValidation);
