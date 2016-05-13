5c9904ae952e5f69659dca43d71f80d4687f7886
Erik <erik.w.gibbons@gmail.com>
2016-05-13 00:02:17 +0000

    No way this was the problem

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,16 +1,18 @@
-function resetCards(){
-   var array = []
-   $('.product-cards').each(function(i){ array[i] = $(this).css('top'); });
-  var u_array = $.unique(array);
-  var pos1 = parseInt(u_array[0]);
-  var pos2 = parseInt(u_array[1]);
-  var cardHeight = parseInt($('.product-cards').first().css('height'));
-  var gutter = 30;
-  if(pos2 - pos1 < cardHeight){
-    for(i in u_array){ 
-      $('.product-cards[style*="top: '+u_array[i]+'"]').css('top', i*(cardHeight+gutter)); 
+$(document).ready(function(){
+  function resetCards(){
+    var array = []
+    $('.product-cards').each(function(i){ array[i] = $(this).css('top'); });
+    var u_array = $.unique(array);
+    var pos1 = parseInt(u_array[0]);
+    var pos2 = parseInt(u_array[1]);
+    var cardHeight = parseInt($('.product-cards').first().css('height'));
+    var gutter = 30;
+    if(pos2 - pos1 < cardHeight){
+      for(i in u_array){ 
+        $('.product-cards[style*="top: '+u_array[i]+'"]').css('top', i*(cardHeight+gutter)); 
+      }
     }
   }
-};
 
-$(document).ready(resetCards());
+  $(document).ready(resetCards());
+});
